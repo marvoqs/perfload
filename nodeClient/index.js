5086c9e99b4cdefd81878c1b7c0d29ef6d7bf9f9
Marek Schwarz <marek.schwarz@designdev.cz>
2021-09-15 11:11:12 +0200

    Init project

---
+++ nodeClient/index.js
@@ -0,0 +1,7 @@
+// The node program that captures local performance data
+// and sends it up to the socket.io server
+// req:
+// - farmhash
+// - socket.io-client
+
+// What do we need to know from node about performance?
