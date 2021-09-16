8cc4e8ec06b4429930c8abf01326f23ca8ff6777
Marek Schwarz <marek.schwarz@designdev.cz>
2021-09-16 16:34:58 +0200

    Finish CPU and Mem components

--- reactclient/src/components/Cpu.js
+++ reactclient/src/components/Cpu.js
@@ -1,15 +1,20 @@
 import drawCircle from "../utilities/drawCircle";
 
 const Cpu = ({ data: { cpuLoad } }) => {
-  const canvas = document.querySelector("canvas");
+  const canvas = document.getElementById("cpuCanvas");
   drawCircle(canvas, cpuLoad);
 
   return (
-    <div className="col-sm3 cpu">
+    <div className="col-sm-3 cpu">
       <h3>CPU Load</h3>
       <div className="canvas-wrapper">
-        <canvas className="canvas"></canvas>
-        <div className="cpu-text">{cpuLoad}</div>
+        <canvas
+          className="canvas"
+          height="200"
+          id="cpuCanvas"
+          width="200"
+        ></canvas>
+        <div className="cpu-text">{cpuLoad}%</div>
       </div>
     </div>
   );
--- reactclient/src/components/Mem.js
+++ reactclient/src/components/Mem.js
@@ -1,5 +1,28 @@
-const Mem = () => {
-  return <div>Mem</div>;
+import drawCircle from "../utilities/drawCircle";
+
+const Mem = ({ data: { freeMem, totalMem, usedMem, memUsage } }) => {
+  const canvas = document.getElementById("memCanvas");
+  const memUsageInPercents = memUsage * 100;
+  const totalMemInGb = totalMem / (1024 * 1024 * 1024);
+  const freeMemInGb = Math.round(freeMem / (1024 * 1024 * 1024));
+  drawCircle(canvas, memUsageInPercents);
+
+  return (
+    <div className="col-sm-3 cpu">
+      <h3>Memory Usage</h3>
+      <div className="canvas-wrapper">
+        <canvas
+          className="canvas"
+          height="200"
+          id="memCanvas"
+          width="200"
+        ></canvas>
+        <div className="cpu-text">{memUsageInPercents}%</div>
+        <div>Total Memory: {totalMemInGb} GB</div>
+        <div>Free Memory: {freeMemInGb} GB</div>
+      </div>
+    </div>
+  );
 };
 
 export default Mem;
